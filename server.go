7036c44a2ca76f1405776775b7a6f6e20f258019
Marc Mendoza <marcmendoza@Marcs-Mac-mini.attlocal.net>
2020-07-15 18:17:52 -0400

    adding new route

--- server.go
+++ server.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+// User object
 type User struct {
 	Name  string
 	Email string
-	Id    string
+	ID    int
 }
 
+// Users array
 type Users []User
 
 func allUsers(w http.ResponseWriter, r *http.Request) {
-	
+
+	users := Users{
+		User{Name: "Marc", Email: "777marc@gmail.com", ID: 1},
+	}
+
+	fmt.Println("all users endpoint")
+	json.NewEncoder(w).Encode(users)
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -24,6 +33,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/users", allUsers)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
